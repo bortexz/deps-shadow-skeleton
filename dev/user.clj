a4c5c1fc5d070d9760a0d09bd05a3aeb874e9342
bertofer <alberto.fernandez.491@gmail.com>
2022-07-08 21:06:20 +0200

    Add CustomREPL connect sequience and nrepl deps alias

--- dev/user.clj
+++ dev/user.clj
@@ -2,6 +2,9 @@
   (:require [shadow.cljs.devtools.api :as shadow]
             [shadow.cljs.devtools.server :as shadow-server]))
 
-(defn start-cljs []
-  (shadow-server/start!)
-  (shadow/watch :app))
+(defn start-cljs
+  ([] (start-cljs :app))
+  ([build-id]
+   (shadow-server/start!)
+   (shadow/watch build-id)
+   (shadow/nrepl-select build-id)))
